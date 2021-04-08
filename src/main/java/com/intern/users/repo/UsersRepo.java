52ec42eddea5204073d6803aa6224916e8ad5cc1
Shrishti Sharma <48645595+shrishti1997@users.noreply.github.com>
2021-04-08 13:15:08 +0530

    Delete src directory

--- src/main/java/com/intern/users/repo/UsersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.intern.users.repo;
-
-import com.intern.users.model.Users;
-import org.springframework.data.couchbase.repository.CouchbaseRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepo extends CouchbaseRepository<Users, String > {
-}
