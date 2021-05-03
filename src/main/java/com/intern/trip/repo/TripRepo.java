ba4f29a7fa0b5d3b25120acd8574983d92be0e8e
Shrishti Sharma <shrishti.nitc@gmail.com>
2021-05-03 15:05:42 +0530

    trip micro service

--- src/main/java/com/intern/trip/repo/TripRepo.java
+++
@@ -1,9 +0,0 @@
-package com.intern.trip.repo;
-
-import com.intern.trip.model.Trip;
-import org.springframework.data.couchbase.repository.CouchbaseRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TripRepo extends CouchbaseRepository<Trip, String> {
-}
